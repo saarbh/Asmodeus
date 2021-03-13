bf42a9edf2f054ea4f4b08af0c01bd1e8ae754f2
saarbh <saar92@gmail.com>
2021-03-13 20:51:51 +0200

    Bug with transpose camera - head tracking doesn't work

---
+++ Assets/Scripts/HandPresence.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    private InputDevice targetDevice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices);
+
+        foreach (var item in devices){
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if(devices.Count > 0){
+            targetDevice = devices[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        if(primaryButtonValue)
+            Debug.Log("Pressing Primary Button");
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+        if(triggerValue > 0.1f)
+            Debug.Log("TriggerValue : " + triggerValue);
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+        if(primary2DAxisValue != Vector2.zero)
+            Debug.Log("Analog Stick : " + primary2DAxisValue);
+    }
+}
