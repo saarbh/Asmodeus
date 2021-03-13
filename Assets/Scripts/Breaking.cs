104ef552791782912acbcc52dd791400a187ff9f
saarbh <saar92@gmail.com>
2021-03-13 14:34:25 +0200

    Cracked Vase + Added input management system for VR

---
+++ Assets/Scripts/Breaking.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Breaking : MonoBehaviour
+{
+    public GameObject destroyedVase;
+void OnMouseDown() {
+    Debug.Log("Clicked");
+    Instantiate(destroyedVase, transform.position, transform.rotation);
+    Destroy(gameObject);
+}
+}
