d7d7048156159a00ee68c2ce3d48f43eafe00374
Ariel Jannai <arieljannai@gmail.com>
2021-03-14 02:13:21 +0200

    spirit animation

---
+++ Assets/Scripts/ParticlesSpirit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesSpirit : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public ParticleSystem spiritFastCloud;
+    public ParticleSystem spiritLineTrail;
+    private int nMoveWaitTime = 6;
+
+    void Start()
+    {
+        //StartCoroutine(MoveSpirit());
+        spiritFastCloud.GetComponent<Animator>();
+    }
+
+    IEnumerator MoveSpirit()
+    {
+        yield return new WaitForSeconds(nMoveWaitTime);
+
+        yield return new WaitForSeconds(nMoveWaitTime);
+
+        yield return new WaitForSeconds(nMoveWaitTime);
+
+        yield return new WaitForSeconds(nMoveWaitTime);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
