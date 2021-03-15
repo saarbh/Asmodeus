72060dbede8240293491e953ee145e878b7853e5
saarbh <saar92@gmail.com>
2021-03-15 12:01:03 +0200

    Audio SoundManager Engine added - right hand

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using UnityEngine.Audio;
+[System.Serializable]
+public class Sound 
+{
+
+public string name;
+
+public AudioClip clip;
+
+public bool loop;
+[Range(0f,1f)]
+public float volume;
+
+[Range(0f,3f)]
+public float pitch;
+
+[HideInInspector]
+public AudioSource source;
+
+}
