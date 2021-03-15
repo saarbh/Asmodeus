098a78339ca8a43b14419793c047b5e5deb61893
saarbh <saar92@gmail.com>
2021-03-15 15:36:04 +0200

    finalize opening cutscene + disable hands interactions while the cutscene

--- Assets/Scripts/AudioEffect.cs
+++ Assets/Scripts/AudioEffect.cs
@@ -5,40 +5,24 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class AudioEffect : XRGrabInteractable
 {
+    public string DialogVoice;
     private Vector3 interactorPosition = Vector3.zero;
+
     private Quaternion interactorRotation = Quaternion.identity;
     
+
     protected override void OnSelectEntering (XRBaseInteractor interactor){
         Debug.Log("Entered");
-        FindObjectOfType<AudioManager>().Play("ShoevAvak");
+        FindObjectOfType<AudioManager>().Play(DialogVoice);
         // base.OnSelectEntering(interactor);
         // StoreInteractor(interactor);
         // MatchAttachmentPoints(interactor);
     }
     
-    private void StoreInteractor(XRBaseInteractor interactor){
-        interactorPosition = interactor.attachTransform.localPosition;
-        interactorRotation = interactor.attachTransform.localRotation;
-    }
-    private void MatchAttachmentPoints(XRBaseInteractor interactor){
-        bool hasAttach = attachTransform != null;
-        interactor.attachTransform.position = hasAttach ? attachTransform.position : transform.position;
-        interactor.attachTransform.rotation = hasAttach ? attachTransform.rotation : transform.rotation;
-    }
     protected override void OnSelectExiting(XRBaseInteractor interactor){
         Debug.Log("Exit");
         // base.OnSelectExiting(interactor);
         // ResetAttacmentPoint(interactor);
         // ClearInteractor(interactor);
     }
-
-    private void ResetAttacmentPoint(XRBaseInteractor interactor){
-        interactor.attachTransform.localPosition = interactorPosition;
-        interactor.attachTransform.localRotation = interactorRotation;
-    }
-    private void ClearInteractor(XRBaseInteractor interactor){
-        interactorPosition = Vector3.zero;
-        interactorRotation = Quaternion.identity;
-    }
-   
 }
